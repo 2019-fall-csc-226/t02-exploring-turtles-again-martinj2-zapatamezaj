51c397229c0bf058507f32d5c7990c7766457c7d
martinj2 <martinj2@berea.edu>
2019-09-04 08:47:54 -0400

    Completed T02, finished the custom turtle file. Committing and pushing to GitHub.

--- Custom Turtle.py
+++ Custom Turtle.py
@@ -1,9 +1,64 @@
 import turtle
 # Brings in the Turtle code
-
 wn = turtle.Screen()
+wn.bgcolor("orange")
 # Creates the background
 my_turtle = turtle.Turtle()
-# Create
+# Creates the turtle
+my_turtle.color("purple")
+my_turtle.penup()                #stops drawing
+my_turtle.forward(-50)            #moves the turtle backwards
+my_turtle.pendown()              #continues drawing
+my_turtle.left(90)               #moves the turtle counterclockwise 90 degrees
+my_turtle.forward(75)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.forward(-100)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(75)
+my_turtle.right(90)
+my_turtle.forward(50)
+my_turtle.penup()
+my_turtle.left(180)
+my_turtle.forward(200)
+my_turtle.pendown()
+my_turtle.left(90)
+my_turtle.forward(75)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.forward(-100)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(75)
+my_turtle.right(90)
+my_turtle.forward(50)
 
 
+tess = turtle.Turtle()
+tess.shape("triangle")
+tess.color("red")
+
+tess.penup()
+size = 20
+for i in range(13):
+    tess.stamp()
+    size = size + 3
+    tess.forward(size)
+    tess.left(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wn.exitonclick()
